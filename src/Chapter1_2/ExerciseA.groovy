1db88474cba9d10082178963cc2d0f6596b504c9
Alexander Worton <kesomir@btinternet.com>
2016-10-02 16:50:51 +0100

    1.2 Exercise A - post-execute

--- src/Chapter1_2/ExerciseA.groovy
+++ src/Chapter1_2/ExerciseA.groovy
@@ -21,10 +21,41 @@ class ExerciseA {
 
     num = 24 / (num / 4)    num: 24 / (2 / 4) : 24 / 0 : ERR
     println num             Line not reached
-     */
+    */
+
+    /*
+    Incorrect answers listed below:
+
+    num = num / 3 * 6       num: 7 / 3 * 6 : 2 * 6 : 12
+    println num             output: 12
+
+        This answer was incorrect, because java converts to floating point as required
+        during the calculation, and only discards when writing to the type. Therefore,
+        7 / 3 holds 2.3333333333, not 2 as in C#. This results in 2.3333333333 * 6 ->
+        13.99999 which the fractional part is discarded when stored as an int.
+
+    num = 24 / (num / 4)    num: 24 / (2 / 4) : 24 / 0 : ERR
+    println num             Line not reached
+
+        For the same reason above, java does not divide by 0, but instead divides by 0.5
+    */
 
     public void run(){
+        int num = 5
+        println num
+        num = num + 2
+        println num
+        num = num / 3 * 6
+        println num
+        println 7 + 15 % 4
+        num = 24 / 3 / 4
+        println num
+        num = 24 / (num / 4)
+        println num
 
+        println ""
+        println "(7 / 3): " + (7 / 3)
+        println "(2 / 4): " + (2 / 4)
     }
 
 }
