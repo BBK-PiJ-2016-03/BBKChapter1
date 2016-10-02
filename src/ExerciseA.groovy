c52614839b522b0c5b9e47b20d30f561f421d69c
Alexander Worton <kesomir@btinternet.com>
2016-10-02 14:44:51 +0100

    Third task

--- src/ExerciseA.groovy
+++ src/ExerciseA.groovy
@@ -11,4 +11,32 @@ class ExerciseA {
         println("Hickory, Dickory, Dock")
     }
 
+    public static void three(int numWords = 2){
+        Stack<String> words = new Stack<>()
+
+        words = collectWords(words, numWords)
+
+        outputWords(words, "\t• ")
+    }
+
+    private static void outputWords(Stack<String> words, String preceedingString){
+        println()
+        println("Your words in reverse order are: ")
+        while(words.size() > 0){
+            println preceedingString + words.pop()
+        }
+    }
+
+    private static Stack<String> collectWords(Stack<String> words, numWords){
+        Scanner sc = new Scanner(System.in)
+
+        for (int i = 1; i <= numWords; i++){
+            println ("Please enter word " + i)
+            String word = sc.next()
+            words.push(word)
+        }
+
+        return words
+    }
+
 }
