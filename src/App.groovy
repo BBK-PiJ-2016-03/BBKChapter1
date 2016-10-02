409728d2fc9d65da76ff0dbf0c11aa0da68afad2
Alexander Worton <kesomir@btinternet.com>
2016-10-02 13:43:36 +0100

    Initial Commit

---
+++ src/App.groovy
@@ -0,0 +1,10 @@
+/**
+ * Created by Alexander Worton on 02/10/2016.
+ */
+class App {
+
+    public static void main(String[] args){
+        ExerciseA run = new ExerciseA()
+        run.one()
+    }
+}
---
+++ src/ExerciseA.groovy
@@ -0,0 +1,10 @@
+/**
+ * Created by Alexander Worton on 02/10/2016.
+ */
+class ExerciseA {
+
+    public static void one(){
+        println("")
+    }
+
+}
