3dc47b387f8148eb40b3667f44caa28cb15e3ee0
Alexander Worton <kesomir@btinternet.com>
2016-10-02 15:33:09 +0100

    Exercise B (Fixed output to spec)

--- src/ExerciseB.groovy
+++ src/ExerciseB.groovy
@@ -5,10 +5,12 @@ class ExerciseB {
     public void conCat(int numWords = 2){
         Scanner sc = new Scanner(System.in)
         String build = ""
-        for (int i = 1; i <= numWords; i++){
-            println "Please enter word " + i
+        for (int i = 0; i < numWords; i++){
+
+            print i < 1 ? "Please key in a word: " : "And now key in another: "
+
             build += sc.next().trim() + " "
         }
-        println build.trim()
+        println "You have typed: " + build.trim()
     }
 }
